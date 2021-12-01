715040ff6abef2e111d599639543dc2c157168f8
jimmiebergmann <jimmiebergmann@gmail.com>
2021-12-01 09:58:33 +0100

    Add day 1.

--- solutions/day_1/day_1.cpp
+++ solutions/day_1/day_1.cpp
@@ -1,21 +1,40 @@
 #include "utility.hpp"
 
-int64_t solve_part_1(const std::vector<std::string>& values)
+size_t solve_part_1(const std::vector<int64_t>& values)
 {
-    return 0;
+    size_t result = 0;
+    for (size_t i = 1; i < values.size(); i++)
+    {
+        if (values[i - 1] < values[i])
+        {
+            ++result;
+        }
+    }
+
+    return result;
 }
 
-int64_t solve_part_2(const std::vector<std::string>& values)
+int64_t solve_part_2(const std::vector<int64_t>& values)
 {
-    return 0;
+    size_t result = 0;
+    for (size_t i = 1; i < values.size() - 2; i++)
+    {
+        const auto prevSum = values[i - 1] + values[i] + values[i + 1];
+        const auto curSum = values[i] + values[i + 1] + values[i + 2];
+
+        if (prevSum < curSum)
+        {
+            ++result;
+        }
+    }
+
+    return result;
 }
 
 int main()
 {
-    const auto input = aoc::read_input<1, std::string>();
-
     aoc::solve_puzzles(
-        input,
+        aoc::read_input<1, int64_t>(true),
         solve_part_1,
         solve_part_2
     );
