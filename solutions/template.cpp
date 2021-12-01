7579287f9e8e5e794a8dfd35640f6279978ab5c6
jimmie.nilsson <jimmie.nilsson@exsitec.se>
2021-12-01 09:58:33 +0100

    Add day 1.

--- solutions/template.cpp
+++ solutions/template.cpp
@@ -12,10 +12,8 @@ int64_t solve_part_2(const std::vector<std::string>& values)
 
 int main()
 {
-    const auto input = aoc::read_input<1, std::string>();
-
     aoc::solve_puzzles(
-        input,
+        aoc::read_input<1, std::string>(),
         solve_part_1,
         solve_part_2
     );
